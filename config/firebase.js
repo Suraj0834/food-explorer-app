cd540fb369abcb5d727f2589f9bf168699b67ce5
Suraj Kumar Singh Kushwaha <119146368+Suraj0834@users.noreply.github.com>
2025-07-09 11:11:56 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -5,13 +5,12 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "food-explorer-app-9fb6a.firebaseapp.com",
-  projectId: "food-explorer-app-9fb6a",
-  storageBucket: "food-explorer-app-9fb6a.firebasestorage.app",
-  messagingSenderId: "1005920493587",
-  appId: "1:1005920493587:web:8322f3d9e3ec7654cc9fe6",
-  measurementId: "G-MXNWVHQYDY"
+  apiKey: "your_actual_api_key_here",
+  authDomain: "your_project_id.firebaseapp.com",
+  projectId: "your_project_id",
+  storageBucket: "your_project_id.appspot.com",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id"
 };
 
 // Initialize Firebase only if no apps exist
